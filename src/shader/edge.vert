2d070a2879f84b964f3a719d7b6ad52b2cdd7921
Hood <hood@mit.edu>
2020-09-27 12:54:34 -0700

    Moved edge vertex shader into separate file, rearranged edge shader stuff a bit.

---
+++ src/shader/edge.vert
@@ -0,0 +1,139 @@
+#version 300 es
+#define M_PI 3.1415926535897932384626433832795
+#define ANGLE_RES 180 // should be same as ANGLE_RESOLUTION
+
+
+
+
+// Note: this variant counts each pixel as 4 distinct floats.
+float getValueByIndexFrom4ChannelTexture(sampler2D tex, int index){
+    int texWidth = textureSize(tex, 0).x;
+    int channel = index % 4;
+    int texOffset = index / 4;
+    int col = texOffset % texWidth;
+    int row = texOffset / texWidth;
+    return texelFetch(tex, ivec2(col, row), 0)[channel];
+}
+
+vec4 getValueByIndexFromTexture(sampler2D tex, int index) {
+    int texWidth = textureSize(tex, 0).x;
+    int col = index % texWidth;
+    int row = index / texWidth;
+    return texelFetch(tex, ivec2(col, row), 0);
+}
+
+
+float getGlyphBoundaryPoint(sampler2D tex, int glyph, float angle){
+    int glyph_index = (int(angle / (2.0 * M_PI) * float(ANGLE_RES)) + ANGLE_RES) % ANGLE_RES;
+    int total_index = ANGLE_RES * glyph + glyph_index;
+    return getValueByIndexFrom4ChannelTexture(tex, total_index);
+}
+
+struct ArrowHeader {
+    float tip_end;
+    float back_end;
+    float visual_tip_end;
+    float visual_back_end;
+    float line_end;
+};
+
+ArrowHeader readArrowHeader(sampler2D arrowHeaderTexture, int arrowHeaderIndex){
+    float tip_end = getValueByIndexFrom4ChannelTexture(arrowHeaderTexture, arrowHeaderIndex);
+    float back_end = getValueByIndexFrom4ChannelTexture(arrowHeaderTexture, arrowHeaderIndex + 1);
+    float visual_tip_end = getValueByIndexFrom4ChannelTexture(arrowHeaderTexture, arrowHeaderIndex + 2);
+    float visual_back_end = getValueByIndexFrom4ChannelTexture(arrowHeaderTexture, arrowHeaderIndex + 3);
+    float line_end = getValueByIndexFrom4ChannelTexture(arrowHeaderTexture, arrowHeaderIndex + 4);
+    return ArrowHeader(tip_end, back_end, visual_tip_end, visual_back_end, line_end);
+}
+
+vec2 getArrowVertex(sampler2D arrowVerticesTexture, int arrow_index, int vertex_index) {
+    return getValueByIndexFromTexture(arrowVerticesTexture, arrow_index + vertex_index).xy;
+}
+
+
+
+uniform mat3x2 uTransformationMatrix;
+uniform vec2 uOrigin;
+uniform vec2 uScale;
+uniform sampler2D uGlyphDataTexture;
+uniform sampler2D uArrowPathTexture;
+// uniform sampler2D uArrowTipPathTexture;
+
+in vec4 aColor;
+in vec2 aStartPosition;
+in vec2 aEndPosition;
+in int aStartGlyph;
+in int aEndGlyph;
+in float aStartGlyphScale;
+in float aEndGlyphScale;
+
+in int aStartArrowNumVertices;
+in int aStartArrowHeaderIndex;
+in int aStartArrowVerticesIndex;
+
+in int aEndArrowNumVertices;
+in int aEndArrowHeaderIndex;
+in int aEndArrowVerticesIndex;
+
+flat out vec4 fColor;
+
+
+ivec2 vertexIndexes[6] = ivec2[](
+    ivec2(0, 0), ivec2(0, 1), ivec2(1, 0),
+    ivec2(0, 1), ivec2(1, 0), ivec2(1, 1)
+);
+
+vec2 testPositions[3] = vec2[](
+    vec2(0.8, 1.34641), vec2(2.2, 1.34641), vec2(1.5, 0.133975)
+);
+
+
+void main() {
+    vec2 transformedStart = uOrigin +  uScale * aStartPosition;
+    vec2 transformedEnd = uOrigin +  uScale * aEndPosition;
+
+    vec2 displacement = normalize(transformedEnd - transformedStart);
+    float angle = atan(displacement.y, displacement.x);
+    float startOffset = aStartGlyphScale * getGlyphBoundaryPoint(uGlyphDataTexture, aStartGlyph, angle);
+    float endOffset = aEndGlyphScale * getGlyphBoundaryPoint(uGlyphDataTexture, aEndGlyph, angle + M_PI);
+
+    vec2 startVec = transformedStart + startOffset * displacement;
+    vec2 endVec = transformedEnd - endOffset * displacement;
+
+    vec2 normal = vec2(-displacement.y, displacement.x);
+
+    vec2 position;
+    if(gl_VertexID < 6){
+        // Line
+        ivec2 vertexIndex = vertexIndexes[gl_VertexID];
+
+        if(vertexIndex.x == 1){
+            normal = - normal;
+        }
+
+        if(vertexIndex.y == 0){
+            position = startVec + normal;
+        } else {
+            position = endVec + normal;
+        }
+    } else if(gl_VertexID < 6 + aStartArrowNumVertices) {
+        // Start arrow
+        int vertex_index = gl_VertexID - 6;
+        mat2 rotationMatrix = mat2(displacement, normal);
+        position = startVec - rotationMatrix * getArrowVertex(uArrowPathTexture, aStartArrowVerticesIndex, vertex_index).xy;
+    } else if(gl_VertexID < 6 + aStartArrowNumVertices + aEndArrowNumVertices) {
+        // End arrow
+        int vertex_index = gl_VertexID - 6 - aStartArrowNumVertices;
+        mat2 rotationMatrix = mat2(displacement, normal);
+        position = endVec + rotationMatrix * getArrowVertex(uArrowPathTexture, aEndArrowVerticesIndex, vertex_index).xy;
+    } else {
+        // Extra throw-away vertices
+        position = vec2(0.0, 0.0);
+    }
+
+    gl_Position = vec4(uTransformationMatrix * vec3(position, 1.0), 0.0, 1.0);
+    fColor = aColor;
+    if(aStartArrowNumVertices == 0){
+        fColor.g = 1.0;
+    }
+}
--- src/shader/edge_shader_test2.rs
+++ src/shader/edge_shader_test2.rs
@@ -16,6 +16,16 @@ use crate::convex_hull::ANGLE_RESOLUTION;
 
 const DATA_ROW_SIZE : usize = 2048;
 
+struct DataTexture<T> {
+    row_size : usize,
+    num_components : u8,
+    data : Vec<T>,
+    used_data : usize,
+    texture : Option<WebGlTexture>,
+    texture_rows : usize,
+}
+
+
 #[derive(Copy, Clone, Debug)]
 enum Type {
     Float,
@@ -45,14 +55,17 @@ const ATTRIBUTES : [(&str, i32, Type); 11] = [
     ("aStartGlyphScale", 1, Type::Float), // start_glyph_scale
     ("aEndGlyphScale", 1, Type::Float), // end_glyph_scale
 
-    ("aStartArrowTipIndex", 1, Type::Short), 
-    ("aEndArrowTipIndex", 1, Type::Short), 
-    ("aStartArrowTipNumVertices", 1, Type::Short), 
-    ("aEndArrowTipNumVertices", 1, Type::Short), 
+    ("aStartArrowNumVertices", 1, Type::Short), 
+    // ("aStartArrowHeaderIndex", 1, Type::Short), 
+    ("aStartArrowVerticesIndex", 1, Type::Short),
+
+    ("aEndArrowNumVertices", 1, Type::Short), 
+    // ("aEndArrowHeaderIndex", 1, Type::Short), 
+    ("aEndArrowVerticesIndex", 1, Type::Short), 
 ];
 
 #[derive(Clone, Copy, Debug)]
-#[repr(C)]
+#[repr(C, align(4))]
 struct EdgeInstance {
     color : Vec4,
     start_position : Point,
@@ -62,10 +75,26 @@ struct EdgeInstance {
     start_glyph_scale : f32,
     end_glyph_scale : f32,
 
-    start_arrow_tip_index : u16,
-    end_arrow_tip_index : u16,
-    start_arrow_tip_num_vertices : u16,
-    end_arrow_tip_num_vertices : u16,
+    start_arrow : ArrowIndices,
+    end_arrow : ArrowIndices
+}
+
+#[derive(Clone, Copy, Debug)]
+#[repr(C)]
+struct ArrowHeader {
+    tip_end : f32,
+    back_end : f32,
+    visual_tip_end : f32,
+    visual_back_end : f32,
+    line_end : f32,
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+#[repr(C)]
+struct ArrowIndices {
+    num_vertices : u16,
+    // header_index : u16,
+    vertices_index : u16
 }
 
 
@@ -124,12 +153,12 @@ pub struct TestEdgeShader {
 
     // Vertices has its length padded to a multiple of DATA_ROW_SIZE so that it will fit correctly into the data_texture
     // so we need to separately store the number of actually used entries separately.
-    tip_map : BTreeMap<String, (u16, u16)>,
+    tip_map : BTreeMap<String, ArrowIndices>,
     arrow_tip_vertices : Vec<Point>,
     num_arrow_tip_vertices : usize,
     max_arrow_tip_num_vertices : usize,
-    arrow_tip_texture : Option<WebGlTexture>,
-    arrow_tip_texture_rows : usize, // This reminds us how big the texture currently is so we know whether we need to resize it.
+    arrow_tip_path_texture : Option<WebGlTexture>,
+    arrow_tip_path_texture_rows : usize, // This reminds us how big the texture currently is so we know whether we need to resize it.
     
 }
 
@@ -142,113 +171,7 @@ impl TestEdgeShader {
     pub fn new(webgl : WebGlWrapper) -> Result<Self, JsValue> {
         let shader = Shader::new(
             webgl.clone(), 
-            r#"#version 300 es
-                #define M_PI 3.1415926535897932384626433832795
-                #define ANGLE_RES 180 // should be same as ANGLE_RESOLUTION
-                uniform mat3x2 uTransformationMatrix;
-                uniform vec2 uOrigin;
-                uniform vec2 uScale;
-                uniform sampler2D uGlyphDataTexture;
-                uniform sampler2D uArrowTipDataTexture;
-
-                in vec4 aColor;
-                in vec2 aStartPosition;
-                in vec2 aEndPosition;
-                in int aStartGlyph;
-                in int aEndGlyph;
-                in float aStartGlyphScale;
-                in float aEndGlyphScale;
-
-                in int aStartArrowTipIndex;
-                in int aEndArrowTipIndex;
-
-                in int aStartArrowTipNumVertices;
-                in int aEndArrowTipNumVertices;
-
-
-                flat out vec4 fColor;
-
-
-                ivec2 vertexIndexes[6] = ivec2[](
-                    ivec2(0, 0), ivec2(0, 1), ivec2(1, 0),
-                    ivec2(0, 1), ivec2(1, 0), ivec2(1, 1)
-                );
-
-                vec2 testPositions[3] = vec2[](
-                    vec2(0.8, 1.34641), vec2(2.2, 1.34641), vec2(1.5, 0.133975)
-                );
-
-                // Note: this variant counts each pixel as 4 distinct floats.
-                float getValueByIndexFrom4ChannelTexture(sampler2D tex, int index){
-                    int texWidth = textureSize(tex, 0).x;
-                    int channel = index % 4;
-                    int texOffset = index / 4;
-                    int col = texOffset % texWidth;
-                    int row = texOffset / texWidth;
-                    return texelFetch(tex, ivec2(col, row), 0)[channel];
-                }
-
-                float getGlyphBoundaryPoint(sampler2D tex, int glyph, float angle){
-                    int glyph_index = (int(angle / (2.0 * M_PI) * float(ANGLE_RES)) + ANGLE_RES) % ANGLE_RES;
-                    int total_index = ANGLE_RES * glyph + glyph_index;
-                    return getValueByIndexFrom4ChannelTexture(tex, total_index);
-                }
-
-                vec4 getValueByIndexFromTexture(sampler2D tex, int index) {
-                    int texWidth = textureSize(tex, 0).x;
-                    int col = index % texWidth;
-                    int row = index / texWidth;
-                    return texelFetch(tex, ivec2(col, row), 0);
-                }
-
-                vec2 getArrowVertex(sampler2D tex, int arrow_index, int vertex_index) {
-                    return getValueByIndexFromTexture(tex, arrow_index + vertex_index).xy;
-                }
-                
-                void main() {
-                    vec2 transformedStart = uOrigin +  uScale * aStartPosition;
-                    vec2 transformedEnd = uOrigin +  uScale * aEndPosition;
-
-                    vec2 displacement = normalize(transformedEnd - transformedStart);
-                    float angle = atan(displacement.y, displacement.x);
-                    float startOffset = aStartGlyphScale * getGlyphBoundaryPoint(uGlyphDataTexture, aStartGlyph, angle);
-                    float endOffset = aEndGlyphScale * getGlyphBoundaryPoint(uGlyphDataTexture, aEndGlyph, angle + M_PI);
-
-                    vec2 startVec = transformedStart + startOffset * displacement;
-                    vec2 endVec = transformedEnd - endOffset * displacement;
-
-                    vec2 normal = vec2(-displacement.y, displacement.x);
-
-                    vec2 position;
-                    if(gl_VertexID < 6){
-                        ivec2 vertexIndex = vertexIndexes[gl_VertexID];
-
-                        if(vertexIndex.x == 1){
-                            normal = - normal;
-                        }
-
-                        if(vertexIndex.y == 0){
-                            position = startVec + normal;
-                        } else {
-                            position = endVec + normal;
-                        }
-                    } else if(gl_VertexID < 6 + aStartArrowTipNumVertices) {
-                        int vertex_index = gl_VertexID - 6;
-                        mat2 rotationMatrix = mat2(displacement, normal);
-                        position = startVec + rotationMatrix * getArrowVertex(uArrowTipDataTexture, aStartArrowTipIndex, vertex_index).xy;
-                    } else if(gl_VertexID < 6 + aStartArrowTipNumVertices + aEndArrowTipNumVertices) {
-                        int vertex_index = gl_VertexID - 6 - aStartArrowTipNumVertices;
-                        mat2 rotationMatrix = mat2(displacement, normal);
-                        position = endVec + rotationMatrix * getArrowVertex(uArrowTipDataTexture, aEndArrowTipIndex, vertex_index).xy;
-                    } else {
-                        position = vec2(0.0, 0.0);
-                    }
- 
-
-                    gl_Position = vec4(uTransformationMatrix * vec3(position, 1.0), 0.0, 1.0);
-                    fColor = aColor;
-                }
-            "#,
+            include_str!("edge.vert"),
             r#"#version 300 es
                 precision highp float;
                 flat in vec4 fColor;
@@ -264,7 +187,7 @@ impl TestEdgeShader {
         set_up_attributes(attribute_state.as_ref(), attributes_buffer.as_ref(), &webgl, &shader)?;
 
         let glyph_boundary_texture = webgl.inner.create_texture();
-        let arrow_tip_texture = webgl.inner.create_texture();
+        let arrow_tip_path_texture = webgl.inner.create_texture();
 
         Ok(Self {
             webgl,
@@ -283,8 +206,8 @@ impl TestEdgeShader {
             arrow_tip_vertices : Vec::new(),
             num_arrow_tip_vertices : 0,
             max_arrow_tip_num_vertices : 0,
-            arrow_tip_texture,
-            arrow_tip_texture_rows : 0,
+            arrow_tip_path_texture,
+            arrow_tip_path_texture_rows : 0,
             
 
             edge_instances : Vec::new(),
@@ -306,18 +229,23 @@ impl TestEdgeShader {
     }
 
     pub fn arrow_tip_data(&mut self, tip_name : String, vertices : &[Point], indices : &[u16], index_offset : u16) {
-        let tip_index = self.num_arrow_tip_vertices;
-        let tip_num_vertices = indices.len();
+        let vertices_index = self.num_arrow_tip_vertices;
+        let num_vertices = indices.len();
         while self.num_arrow_tip_vertices + indices.len() >= self.arrow_tip_vertices.len() {
             self.arrow_tip_vertices.extend_from_slice(&[Point::new(0.0, 0.0); DATA_ROW_SIZE]);
         }
-        self.arrow_tip_vertices.splice(self.num_arrow_tip_vertices .. self.num_arrow_tip_vertices + tip_num_vertices, 
+        self.arrow_tip_vertices.splice(self.num_arrow_tip_vertices .. self.num_arrow_tip_vertices + num_vertices, 
             indices.iter().map(|&i| vertices[(i - index_offset) as usize])
         ).for_each(drop);
-        self.num_arrow_tip_vertices += tip_num_vertices;
+        self.num_arrow_tip_vertices += num_vertices;
         // log!("glyph data : vertices: {:?}", &self.arrow_tip_vertices[0..self.num_vertices]);
-        self.max_arrow_tip_num_vertices = self.max_arrow_tip_num_vertices.max(tip_num_vertices);
-        self.tip_map.insert(tip_name, (tip_index.try_into().unwrap(), tip_num_vertices.try_into().unwrap()));
+        self.max_arrow_tip_num_vertices = self.max_arrow_tip_num_vertices.max(num_vertices);
+        let arrow_indices = ArrowIndices {
+            num_vertices : num_vertices as u16,
+            vertices_index : vertices_index as u16,
+            // header_index
+        };
+        self.tip_map.insert(tip_name, arrow_indices);
     }
 
 
@@ -327,8 +255,8 @@ impl TestEdgeShader {
         start_glyph_scale : f32, end_glyph_scale : f32,
         start_tip : Option<&str>, end_tip : Option<&str>
     ){
-        let (start_arrow_tip_index, start_arrow_tip_num_vertices) = start_tip.map(|tip| self.tip_map[tip]).unwrap_or((!0, 0));
-        let (end_arrow_tip_index, end_arrow_tip_num_vertices) = end_tip.map(|tip| self.tip_map[tip]).unwrap_or((!0, 0));
+        let start_arrow = start_tip.map(|tip| self.tip_map[tip]).unwrap_or_default();
+        let end_arrow = end_tip.map(|tip| self.tip_map[tip]).unwrap_or_default();
         self.edge_instances.push(EdgeInstance {
             color : Vec4::new(0.0, 0.0, 0.0, 1.0),
             start_position : start,
@@ -338,11 +266,8 @@ impl TestEdgeShader {
             start_glyph_scale,
             end_glyph_scale,
 
-            start_arrow_tip_index,
-            start_arrow_tip_num_vertices,
-
-            end_arrow_tip_index,
-            end_arrow_tip_num_vertices,
+            start_arrow,
+            end_arrow
         })
     }
 
@@ -350,7 +275,7 @@ impl TestEdgeShader {
     fn set_buffer_data(&self){
         self.webgl.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, self.attributes_buffer.as_ref());
         let u8_len = self.edge_instances.len() * std::mem::size_of::<EdgeInstance>();
-        log!("self.glyph_instances : {:?}", self.edge_instances);
+        log!("self.edge_instances : {:?}", self.edge_instances);
 
         let u8_ptr = self.edge_instances.as_ptr() as *mut u8;
         unsafe {
@@ -414,19 +339,19 @@ impl TestEdgeShader {
         Ok(())
     }
 
-    fn ensure_arrow_tip_texture_size(&mut self){
+    fn ensure_arrow_tip_path_texture_size(&mut self){
         let num_rows = self.arrow_tip_vertices.len() / DATA_ROW_SIZE;
-        if num_rows <= self.arrow_tip_texture_rows {
+        if num_rows <= self.arrow_tip_path_texture_rows {
             return;
         }
-        self.webgl.delete_texture(self.arrow_tip_texture.as_ref());
-        self.arrow_tip_texture = self.allocate_texture(DATA_ROW_SIZE as i32, num_rows as i32, WebGl2RenderingContext::RG32F);
+        self.webgl.delete_texture(self.arrow_tip_path_texture.as_ref());
+        self.arrow_tip_path_texture = self.allocate_texture(DATA_ROW_SIZE as i32, num_rows as i32, WebGl2RenderingContext::RG32F);
     }
 
-    fn set_arrow_tip_texture_data(&self) -> Result<(), JsValue> {
+    fn set_arrow_tip_path_texture_data(&self) -> Result<(), JsValue> {
         let num_rows = self.arrow_tip_vertices.len() / DATA_ROW_SIZE;
         let len = self.arrow_tip_vertices.len() * std::mem::size_of::<Point>() / std::mem::size_of::<f32>();
-        self.webgl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, self.arrow_tip_texture.as_ref());
+        self.webgl.bind_texture(WebGl2RenderingContext::TEXTURE_2D, self.arrow_tip_path_texture.as_ref());
         unsafe {
             let array_view = js_sys::Float32Array::view_mut_raw(self.arrow_tip_vertices.as_ptr() as *mut f32, len);
             self.set_texture_data(DATA_ROW_SIZE as i32, num_rows as i32, WebGl2RenderingContext::RG, WebGl2RenderingContext::FLOAT, array_view)?;
@@ -441,8 +366,8 @@ impl TestEdgeShader {
         self.ensure_glyph_texture_size();
         self.set_glyph_texture_data()?;
         self.webgl.active_texture(WebGl2RenderingContext::TEXTURE1);
-        self.ensure_arrow_tip_texture_size();
-        self.set_arrow_tip_texture_data()?;
+        self.ensure_arrow_tip_path_texture_size();
+        self.set_arrow_tip_path_texture_data()?;
         self.webgl.bind_vertex_array(None);
         Ok(())
     }
@@ -452,7 +377,7 @@ impl TestEdgeShader {
         self.shader.use_program();
         self.webgl.bind_vertex_array(self.attribute_state.as_ref());
         self.shader.set_uniform_int("uGlyphDataTexture", 0);
-        self.shader.set_uniform_int("uArrowTipDataTexture", 1);
+        self.shader.set_uniform_int("uArrowPathTexture", 1);
         self.shader.set_uniform_transform("uTransformationMatrix", transform);
         self.shader.set_uniform_point("uOrigin", origin);
         self.shader.set_uniform_point("uScale", scale);
