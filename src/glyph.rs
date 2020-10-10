12938cccaeb181655c4bc145135c7b5831f94b8c
Hood <hood@mit.edu>
2020-10-09 20:11:09 -0700

    Made GlyphBuilder.

--- src/glyph.rs
+++ src/glyph.rs
@@ -134,16 +134,23 @@ fn lyon_path_to_footile_path<T : Iterator<Item=PathEvent>>(path : T) -> Vec<Path
     }).collect()
 }
 
-#[wasm_bindgen]
-#[derive(Clone)]
-pub struct Glyph {
-    paths : Rc<Vec<(Vec<PathEvent>, Option<StrokeOptions>, Option<FillOptions>)>>,
-    convex_hull : Rc<ConvexHull>,
-    pub(crate) uuid : Uuid,
+struct GlyphComponent {
+    path : Vec<PathEvent>,
+    stroke : Option<StrokeOptions>, 
+    fill : Option<FillOptions>,
 }
 
+
 #[wasm_bindgen]
-impl Glyph {
+pub struct GlyphBuilder {
+    paths : Vec<GlyphComponent>,
+    bounding_box : Box2D<f32>,
+    convex_hull_path_idx : usize
+}
+
+
+#[wasm_bindgen]
+impl GlyphBuilder {
     pub fn from_stix(character : &str) -> Self {
         let path : Vec<_> = STIX_FONT.render(
             character,
@@ -151,81 +158,89 @@ impl Glyph {
             font::TextAlign::Center
         ).0.collect();
         let bounding_box = pathop_bounding_box(path.iter());
+        let component = GlyphComponent {
+            path : footile_path_to_lyon_path(path.iter().copied()),
+            stroke : Some(StrokeOptions::default().with_line_width(2.0).with_tolerance(0.2)),
+            fill : Some(FillOptions::default().with_tolerance(0.2)),
+        };
         Self {
-            paths : Rc::new(vec![(
-                footile_path_to_lyon_path(path.iter().cloned()),
-                Some(StrokeOptions::default().with_line_width(2.0).with_tolerance(0.2)), 
-                Some(FillOptions::default().with_tolerance(0.2))
-            )]),
-            convex_hull : Rc::new(ConvexHull::from_path(path, bounding_box)),
-            uuid : Uuid::new_v4()
+            paths : vec![component],
+            bounding_box,
+            convex_hull_path_idx : 0,
         }
     }
 
-    pub fn from_stix_boxed(character : &str, padding : f32) -> Self {
+    pub fn empty() -> Self {
+        Self {
+            paths : vec![],
+            bounding_box : Box2D::new(point(0.0, 0.0), point(0.0, 0.0)),
+            convex_hull_path_idx : 0,
+        }
+    }
+
+    pub fn boxed(&mut self, padding : f32) {
         let padding = padding / 100.0;
-        let path : Vec<_> = STIX_FONT.render(
-            character,
-            (512.0 - 64.0) / FONT_SIZE,
-            font::TextAlign::Center
-        ).0.collect();
-        let bounding_box = pathop_bounding_box(path.iter()).inflate(padding, padding);
-        let Point { x : xmin, y : ymin, ..} = bounding_box.min;
-        let Point { x : xmax, y : ymax, ..} = bounding_box.max;
+        self.bounding_box = self.bounding_box.inflate(padding, padding);
+        let Point { x : xmin, y : ymin, ..} = self.bounding_box.min;
+        let Point { x : xmax, y : ymax, ..} = self.bounding_box.max;
         let box_path = Path2D::default().absolute()
             .move_to(xmin, ymin)
             .line_to(xmax, ymin)
             .line_to(xmax, ymax)
             .line_to(xmin, ymax)
             .close().finish();
-        let convex_hull = Rc::new(ConvexHull::from_path(box_path.iter().cloned(), bounding_box));
-        Self {
-            paths : Rc::new(vec![(
-                footile_path_to_lyon_path(path.iter().cloned()), 
-                Some(StrokeOptions::default().with_line_width(2.0).with_tolerance(0.2)), 
-                Some(FillOptions::default().with_tolerance(0.2))
-            ), (
-                footile_path_to_lyon_path(box_path.iter().cloned()), 
-                Some(StrokeOptions::default().with_line_width(4.0).with_tolerance(0.2)),
-                None, 
-            )]),
-            convex_hull,
-            uuid : Uuid::new_v4()
-        }
+        let component = GlyphComponent {
+            path : footile_path_to_lyon_path(box_path.iter().copied()),
+            stroke : Some(StrokeOptions::default().with_line_width(4.0).with_tolerance(0.2)),
+            fill : None,
+        };        
+        self.convex_hull_path_idx = self.paths.len();
+        self.paths.push(component);
     }
 
-    pub fn from_stix_circled(character : &str, padding : f32) -> Self {
+    pub fn circled(&mut self, padding : f32) {
         let padding = padding / 100.0;
-        let path : Vec<_> = STIX_FONT.render(
-            character,
-            (512.0 - 64.0) / FONT_SIZE,
-            font::TextAlign::Center
-        ).0.collect();
-        let bounding_box = pathop_bounding_box(path.iter()).inflate(padding, padding);
+        let bounding_box = self.bounding_box.inflate(padding, padding);
         let radius = bounding_box.min.distance_to(bounding_box.max)/2.0;
         let center = bounding_box.min.lerp(bounding_box.max, 0.5);
-        let bounding_box = Box2D::new(center - vector(radius, radius), center + vector(radius, radius));
+        self.bounding_box = Box2D::new(center - vector(radius, radius), center + vector(radius, radius));
         let mut circle_path = Path::builder();
         circle_path.move_to(center - vector(radius, 0.0));
         circle_path.arc(center, vector(radius, radius), Angle::two_pi(), Angle::zero());
         circle_path.close();
         let circle_path : Vec<_> = circle_path.build().iter().collect();
-        let convex_hull = Rc::new(ConvexHull::from_path(lyon_path_to_footile_path(circle_path.iter().cloned()), bounding_box));
-        Self {
-            paths : Rc::new(vec![(
-                footile_path_to_lyon_path(path.iter().cloned()), 
-                Some(StrokeOptions::default().with_line_width(2.0).with_tolerance(0.2)), 
-                Some(FillOptions::default().with_tolerance(0.2))
-            ), (
-                circle_path, 
-                Some(StrokeOptions::default().with_line_width(4.0).with_tolerance(0.2)),
-                None, 
-            )]),
+        let component = GlyphComponent {
+            path : circle_path,
+            stroke : Some(StrokeOptions::default().with_line_width(4.0).with_tolerance(0.2)),
+            fill : None,
+        };            
+        self.convex_hull_path_idx = self.paths.len();
+        self.paths.push(component);
+    }
+
+    pub fn build(self) -> Glyph {
+        let GlyphBuilder { paths, bounding_box, convex_hull_path_idx } = self;
+        let convex_hull = Rc::new(ConvexHull::from_path(lyon_path_to_footile_path(paths[convex_hull_path_idx].path.iter().copied()), bounding_box));
+        let paths = Rc::new(paths);
+        Glyph { 
+            paths,
             convex_hull,
             uuid : Uuid::new_v4()
         }
     }
+}
 
+
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct Glyph {
+    paths : Rc<Vec<GlyphComponent>>,
+    convex_hull : Rc<ConvexHull>,
+    pub(crate) uuid : Uuid
+}
+
+impl Glyph { 
     pub(crate) fn tessellate_fill(&self,
         buffers : &mut VertexBuffers<Point, u16>,
         scale : f32
@@ -233,7 +248,7 @@ impl Glyph {
         let mut vertex_builder = geometry_builder::simple_builder(buffers);
         let mut fill_tessellator = FillTessellator::new();
         let transform = Transform::identity().then_translate(- self.convex_hull.center().to_vector()).then_scale(scale, scale);
-        for &(ref path, _stroke, fill) in self.paths.iter() {
+        for &GlyphComponent { ref path, stroke : _stroke,  fill } in self.paths.iter() {
             if let Some(options) = fill {
                 let path = path.iter().copied().transformed(&transform);
                 fill_tessellator.tessellate(path, &options, &mut vertex_builder).map_err(convert_error)?;
@@ -249,7 +264,7 @@ impl Glyph {
         let mut vertex_builder = geometry_builder::simple_builder(buffers);
         let mut stroke_tessellator = StrokeTessellator::new();
         let transform = Transform::identity().then_translate(- self.convex_hull.center().to_vector()).then_scale(scale, scale);
-        for &(ref path, stroke,  _fill) in &*self.paths {
+        for &GlyphComponent { ref path, stroke, fill : _fill } in &*self.paths {
             if let Some(options) = stroke {
                 let path = path.iter().copied().transformed(&transform);
                 stroke_tessellator.tessellate(path, &options, &mut vertex_builder).map_err(convert_error)?;
